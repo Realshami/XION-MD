76e98fe1cbb3e044e66db730934d47adb9895426
Kkushanshaminda <113777967+Kkushanshaminda@users.noreply.github.com>
2025-07-06 12:36:26 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -35,8 +35,8 @@ async (conn, mek, m, { from, sender, reply }) => {
                 forwardingScore: 1000,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363388320701164@newsletter',
-                    newsletterName: 'JesterTechX',
+                    newsletterJid: '@newsletter',
+                    newsletterName: 'XION',
                     serverMessageId: 143
                 }
             }
