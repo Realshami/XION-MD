b04f73edd4bb832985d7800cc1fad10a995c57b7
Kkushanshaminda <113777967+Kkushanshaminda@users.noreply.github.com>
2025-07-06 14:25:17 +0530

    Update tool-voice.js

--- plugins/tool-voice.js
+++ plugins/tool-voice.js
@@ -73,7 +73,7 @@ async (conn, mek, m, {
 
         // Send menu message with image
         const sentMsg = await conn.sendMessage(from, {  
-            image: { url: "https://files.catbox.moe/3y5w8z.jpg" },
+            image: { url: "https://i.ibb.co/Y46jgcpL/2289.jpg" },
             caption: menuText
         }, { quoted: m });
 
